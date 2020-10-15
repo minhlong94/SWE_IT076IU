1ba7f585a72814ae5d18113cf9e0396c083e63a3
Doki <minhdiep1280@gmail.com>
2020-10-15 21:34:25 +0700

    Add docstring for Category

---
+++ src/components/Category.py
@@ -0,0 +1,16 @@
+class Category:
+    """
+    Class Category is where we categorize our products
+
+    Args:
+        category_id (str): is used for identifying category
+        category_name (str): name of the category
+
+    Attributes:
+        _category_id: where we store category_id
+        _category_name: where we store category_name
+    """
+
+    def __init__(self, category_id, category_name):
+        _category_id = category_id
+        _category_name = category_name
