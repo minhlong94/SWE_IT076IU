1b3ddb46eb25b9acfc2063a6fdbc2906508ae3c4
Doki <52833936+Doki064@users.noreply.github.com>
2020-11-26 16:50:14 +0700

    Revert "Update Item.py"
    This reverts commit 166093bd8ccdaf43f7c92add0b02d2bff04e622c.

--- src/components/Item.py
+++ src/components/Item.py
@@ -1,28 +1,26 @@
 class Item:
-    """Class Item is where we define product's info in our system.
+    """
+    Class Item is where we define product's info in our system.
 
     Args:
         item_id (str): is used for identifying item.
         item_name (str): is the name of the item.
         category_id (str): is used for specifying the category of the item.
 
-    Raises:
-        TypeError: if item_id, item_name or category_id is empty or None.
-
     Attributes:
-        item_id: where we store item_id.
-        item_name: where we store item_name.
-        category_id: where we store category_id.
+        _item_id: where we store item_id.
+        _item_name: where we store item_name.
+        _category_id: where we store category_id.
     """
 
     def __init__(self, item_id, item_name, category_id):
-        if not item_id:
+        if item_id is None:
             raise TypeError("Argument 'item_id' is required!")
-        if not item_name:
+        if item_name is None:
             raise TypeError("Argument 'item_name' is required!")
-        if not category_id:
+        if category_id is None:
             raise TypeError("Argument 'category_id' is required!")
 
-        self.item_id = item_id
-        self.item_name = item_name
-        self.category_id = category_id
+        self._item_id = item_id
+        self._item_name = item_name
+        self._category_id = category_id
