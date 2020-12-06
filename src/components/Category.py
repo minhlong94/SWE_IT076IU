2651027d7a8ecdbdd66a2f892f8df53fd68fac90
Doki064 <52833936+Doki064@users.noreply.github.com>
2020-12-07 02:27:31 +0700

    Update Category.py

--- src/components/Category.py
+++ src/components/Category.py
@@ -1,41 +1,46 @@
-from src.components.Item import Item
+import sqlite3
 
 
-class Category:
-    """Class Category is where we categorize our products.
+def insert(connection, category_id, category_name):
+    """Add a new Category to the database.
 
     Args:
-        category_id (str): is used for identifying category.
-        category_name (str): is the name of the category.
-
-    Raises:
-        TypeError: if category_id or category_name is empty or None.
-
-    Attributes:
-        category_id: where we store category id.
-        category_name: where we store category name.
-        items: a list of items belonging to the category.
+        connection (sqlite3.Connection)
+        category_id (str)
+        category_name (str)
     """
 
-    def __init__(self, category_id, category_name):
-        if not category_id:
-            raise TypeError("Argument 'category_id' is required!")
-        if not category_name:
-            raise TypeError("Argument 'category_name' is required!")
+    if not category_id:
+        raise TypeError("Argument 'category_id' is required!")
+    if not category_name:
+        raise TypeError("Argument 'category_name' is required!")
 
-        self.category_id = category_id
-        self.category_name = category_name
-        self.items = set()
+    cur = connection.cursor()
+    cur.execute('''INSERT INTO Category (categoryID, categoryName) VALUES (?,?)''', (category_id, category_name))
+    connection.commit()
 
-    def add_item_to_category(self, item):
-        """Adds an item to the category.
 
-        Args:
-            item (Item)
-        """
+def delete_by_id(connection, category_id):
+    if not category_id:
+        raise TypeError("Argument 'category_id' is required!")
 
-        self.items.add(item)
+    cur = connection.cursor()
+    cur.execute('''DELETE FROM Category WHERE categoryID = ?''', category_id)
+    connection.commit()
 
-    def print_items(self):
-        # Prints the list of items in the category.
-        print(self.items)
+
+def delete_by_name(connection, category_name):
+    if not category_name:
+        raise TypeError("Argument 'category_name' is required!")
+
+    cur = connection.cursor()
+    cur.execute('''DELETE FROM Category WHERE categoryName = ?''', category_name)
+    connection.commit()
+
+
+def search_by_id(connection, category_id):
+    if not category_id:
+        raise TypeError("Argument 'category_id' is required!")
+
+    cur = connection.cursor()
+    cur.execute('''SELECT * FROM Category WHERE categoryID = ?''', category_id)
