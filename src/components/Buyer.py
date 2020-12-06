f41fe361d169ea8907f3be389dfb7c6e2f337616
Doki064 <52833936+Doki064@users.noreply.github.com>
2020-12-07 02:27:27 +0700

    Update Buyer.py

--- src/components/Buyer.py
+++ src/components/Buyer.py
@@ -1,23 +1,46 @@
-class Buyer:
-    """Class Buyer is where we manage and store info about whom we get the supply from.
+import sqlite3
+
+
+def insert(connection, buyer_id, buyer_name):
+    """Add a new buyer to the database.
 
     Args:
-        buyer_id (str): is used for identifying and differentiating buyer.
-        buyer_name (str): is the name of the organization or individual who is the buyer.
-
-    Raises:
-        TypeError: if buyer_id or buyer_name is empty or None.
-
-    Attributes:
-        buyer_id: where we store buyer_id.
-        buyer_name: where we store buyer_name.
+        connection (sqlite3.Connection)
+        buyer_id (str)
+        buyer_name (str)
     """
 
-    def __init__(self, buyer_id, buyer_name):
-        if not buyer_id:
-            raise TypeError("Argument 'buyer_id' is required!")
-        if not buyer_id:
-            raise TypeError("Argument 'buyer_name' is required!")
+    if not buyer_id:
+        raise TypeError("Argument 'buyer_id' is required!")
+    if not buyer_name:
+        raise TypeError("Argument 'buyer_name' is required!")
 
-        self.buyer_id = buyer_id
-        self.buyer_name = buyer_name
+    cur = connection.cursor()
+    cur.execute('''INSERT INTO Buyer (buyerID, buyerName) VALUES (?,?)''', (buyer_id, buyer_name))
+    connection.commit()
+
+
+def delete_by_id(connection, buyer_id):
+    if not buyer_id:
+        raise TypeError("Argument 'buyer_id' is required!")
+
+    cur = connection.cursor()
+    cur.execute('''DELETE FROM Buyer WHERE buyerID = ?''', buyer_id)
+    connection.commit()
+
+
+def delete_by_name(connection, buyer_name):
+    if not buyer_name:
+        raise TypeError("Argument 'buyer_name' is required!")
+
+    cur = connection.cursor()
+    cur.execute('''DELETE FROM Buyer WHERE buyerName = ?''', buyer_name)
+    connection.commit()
+
+
+def search_by_id(connection, buyer_id):
+    if not buyer_id:
+        raise TypeError("Argument 'buyer_id' is required!")
+
+    cur = connection.cursor()
+    cur.execute('''SELECT * FROM Buyer WHERE buyerID = ?''', buyer_id)
