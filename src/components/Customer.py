de897781a172c7af13a07791002e681817923757
Doki064 <52833936+Doki064@users.noreply.github.com>
2020-12-07 02:27:34 +0700

    Update Customer.py

--- src/components/Customer.py
+++ src/components/Customer.py
@@ -1,23 +1,46 @@
-class Customer:
-    """Class Customer is where we manage and store info about who registering into our system.
+import sqlite3
+
+
+def insert(connection, customer_id, customer_name):
+    """Add a new Customer to the database.
 
     Args:
-        customer_id (str): is used for identifying and differentiating customer.
-        customer_name (str): is the name of the organization or individual who is the customer.
-
-    Raises:
-        TypeError: if customer_id or customer_name is empty or None.
-
-    Attributes:
-        customer_id: where we store customer_id.
-        customer_name: where we store customer_name.
+        connection (sqlite3.Connection)
+        customer_id (str)
+        customer_name (str)
     """
 
-    def __init__(self, customer_id, customer_name):
-        if not customer_id:
-            raise TypeError("Argument 'customer_id' is required!")
-        if not customer_name:
-            raise TypeError("Argument 'customer_name' is required!")
+    if not customer_id:
+        raise TypeError("Argument 'customer_id' is required!")
+    if not customer_name:
+        raise TypeError("Argument 'customer_name' is required!")
 
-        self.customer_id = customer_id
-        self.customer_name = customer_name
+    cur = connection.cursor()
+    cur.execute('''INSERT INTO Customer (customerID, customerName) VALUES (?,?)''', (customer_id, customer_name))
+    connection.commit()
+
+
+def delete_by_id(connection, customer_id):
+    if not customer_id:
+        raise TypeError("Argument 'customer_id' is required!")
+
+    cur = connection.cursor()
+    cur.execute('''DELETE FROM Customer WHERE customerID = ?''', customer_id)
+    connection.commit()
+
+
+def delete_by_name(connection, customer_name):
+    if not customer_name:
+        raise TypeError("Argument 'customer_name' is required!")
+
+    cur = connection.cursor()
+    cur.execute('''DELETE FROM Customer WHERE customerName = ?''', customer_name)
+    connection.commit()
+
+
+def search_by_id(connection, customer_id):
+    if not customer_id:
+        raise TypeError("Argument 'customer_id' is required!")
+
+    cur = connection.cursor()
+    cur.execute('''SELECT * FROM Customer WHERE customerID = ?''', customer_id)
