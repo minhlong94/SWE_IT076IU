90fa78c4daa9d5f3928db310b5931226f24430d6
Doki064 <52833936+Doki064@users.noreply.github.com>
2020-12-07 02:36:45 +0700

    Update database

--- src/database/sql.py
+++ src/database/sql.py
@@ -6,32 +6,32 @@ cur = con.cursor()
 cur.execute('''DROP TABLE IF EXISTS Customer''')
 cur.execute('''CREATE TABLE Customer
 (
-  customerID VARCHAR NOT NULL,
-  customerName VARCHAR NOT NULL,
+  customerID VARCHAR(50) NOT NULL,
+  customerName VARCHAR(500) NOT NULL,
   PRIMARY KEY (customerID)
 )''')
 
 cur.execute('''DROP TABLE IF EXISTS Category''')
 cur.execute('''CREATE TABLE Category
 (
-  categoryID VARCHAR NOT NULL,
-  categoryName VARCHAR NOT NULL,
+  categoryID VARCHAR(50) NOT NULL,
+  categoryName VARCHAR(500) NOT NULL,
   PRIMARY KEY (categoryID)
 )''')
 
 cur.execute('''DROP TABLE IF EXISTS Buyer''')
 cur.execute('''CREATE TABLE Buyer
 (
-  buyerID VARCHAR NOT NULL,
-  buyerName VARCHAR NOT NULL,
+  buyerID VARCHAR(50) NOT NULL,
+  buyerName VARCHAR(500) NOT NULL,
   PRIMARY KEY (buyerID)
 )''')
 
 cur.execute('''DROP TABLE IF EXISTS Inventory''')
 cur.execute('''CREATE TABLE Inventory
 (
-  inventoryID VARCHAR NOT NULL,
-  inventoryName VARCHAR NOT NULL,
+  inventoryID VARCHAR(50) NOT NULL,
+  inventoryName VARCHAR(500) NOT NULL,
   PRIMARY KEY (inventoryID)
 )''')
 
@@ -39,9 +39,9 @@ cur.execute('''DROP TABLE IF EXISTS Imports''')
 cur.execute('''CREATE TABLE Imports
 (
   importDate DATE NOT NULL,
-  importID VARCHAR NOT NULL,
-  inventoryID VARCHAR NOT NULL,
-  buyerID VARCHAR NOT NULL,
+  importID VARCHAR(50) NOT NULL,
+  inventoryID VARCHAR(50) NOT NULL,
+  buyerID VARCHAR(50) NOT NULL,
   PRIMARY KEY (importID),
   FOREIGN KEY (inventoryID) REFERENCES Inventory(inventoryID),
   FOREIGN KEY (buyerID) REFERENCES Buyer(buyerID)
@@ -50,11 +50,11 @@ cur.execute('''CREATE TABLE Imports
 cur.execute('''DROP TABLE IF EXISTS Transactions''')
 cur.execute('''CREATE TABLE Transactions
 (
-  transactionID VARCHAR NOT NULL,
+  transactionID VARCHAR(50) NOT NULL,
   createDate DATE NOT NULL,
-  status VARCHAR NOT NULL,
-  customerID VARCHAR NOT NULL,
-  inventoryID VARCHAR NOT NULL,
+  transactionStatus VARCHAR(20) NOT NULL,
+  customerID VARCHAR(50) NOT NULL,
+  inventoryID VARCHAR(50) NOT NULL,
   PRIMARY KEY (transactionID),
   FOREIGN KEY (customerID) REFERENCES Customer(customerID),
   FOREIGN KEY (inventoryID) REFERENCES Inventory(inventoryID)
@@ -63,11 +63,11 @@ cur.execute('''CREATE TABLE Transactions
 cur.execute('''DROP TABLE IF EXISTS Item''')
 cur.execute('''CREATE TABLE Item
 (
-  itemName VARCHAR NOT NULL,
-  itemID VARCHAR NOT NULL,
+  itemName VARCHAR(500) NOT NULL,
+  itemID VARCHAR(50) NOT NULL,
   quantity INT NOT NULL,
-  categoryID VARCHAR NOT NULL,
-  inventoryID VARCHAR NOT NULL,
+  categoryID VARCHAR(50) NOT NULL,
+  inventoryID VARCHAR(50) NOT NULL,
   PRIMARY KEY (itemID),
   FOREIGN KEY (categoryID) REFERENCES Category(categoryID),
   FOREIGN KEY (inventoryID) REFERENCES Inventory(inventoryID)
@@ -77,8 +77,8 @@ cur.execute('''DROP TABLE IF EXISTS TransactionsDetail''')
 cur.execute('''CREATE TABLE TransactionsDetail
 (
   transactionAmount INT NOT NULL,
-  transactionID VARCHAR NOT NULL,
-  itemID VARCHAR NOT NULL,
+  transactionID VARCHAR(50) NOT NULL,
+  itemID VARCHAR(50) NOT NULL,
   PRIMARY KEY (transactionID),
   FOREIGN KEY (transactionID) REFERENCES Transactions(transactionID),
   FOREIGN KEY (itemID) REFERENCES Item(itemID)
@@ -88,8 +88,8 @@ cur.execute('''DROP TABLE IF EXISTS ImportsDetail''')
 cur.execute('''CREATE TABLE ImportsDetail
 (
   importAmount INT NOT NULL,
-  itemID VARCHAR NOT NULL,
-  importID VARCHAR NOT NULL,
+  itemID VARCHAR(50) NOT NULL,
+  importID VARCHAR(50) NOT NULL,
   PRIMARY KEY (itemID, importID),
   FOREIGN KEY (itemID) REFERENCES Item(itemID),
   FOREIGN KEY (importID) REFERENCES Imports(importID)
