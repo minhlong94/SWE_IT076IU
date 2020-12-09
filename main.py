0e5c5e565b6103e1c5a2e66be5186a127564412c
Doki064 <52833936+Doki064@users.noreply.github.com>
2020-12-09 17:15:22 +0700

    Update main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import base64
 import hashlib
+from time import sleep
 
 import bcrypt
 import streamlit as st
@@ -8,7 +9,19 @@ from src.gui.main_page import MainPage
 from src.gui.menu import Menu
 
 
+def login_section():
+    st.sidebar.title("LOGIN SECTION")
+    st.sidebar.write(""" 
+        ## **WARNING: AUTHORIZED ACCESS ONLY**
+        Input your administrator password on the left sidebar, then press "Enter" to login.
+    """)
+    input_password = st.sidebar.text_input("Input administrator password: ", type="password", value="")
+    return input_password
+
+
 def main():
+    with open("src/encryption/check_login", "rb") as f:
+        check_login = f.readline()
     with open("src/encryption/hash_pw.txt", "rb") as f:
         hashed_password = f.read()
 
@@ -16,22 +29,27 @@ def main():
 
     main_page = MainPage()
     main_page.call()
-    st.sidebar.title("LOGIN SECTION")
-    text = """ 
-           ## **WARNING: AUTHORIZED ACCESS ONLY**
-           Input your administrator password on the left sidebar, then press "Enter" to login.
-           """
-    st.sidebar.markdown(text)
-    input_password = st.sidebar.text_input("Input administrator password: ", type="password", value="")
-    st.sidebar.write("Note: this is a collapsible sidebar.")
 
-    while not bcrypt.checkpw(base64.b64encode(hashlib.sha256(input_password.encode("utf8")).digest()), hashed_password):
-        st.sidebar.warning("Wrong password!")
-        st.stop()
+    if check_login != hashlib.md5("IS_LOGGED_IN".encode()).digest():
+        input_password = login_section()
+        if st.sidebar.button("Login"):
+            if not bcrypt.checkpw(base64.b64encode(hashlib.sha256(input_password.encode("utf8")).digest()),
+                                  hashed_password):
+                st.sidebar.warning("Wrong password!")
+                st.stop()
+            else:
+                with open("src/encryption/check_login", "wb") as f:
+                    f.write(hashlib.md5("IS_LOGGED_IN".encode()).digest())
+                st.experimental_rerun()
     else:
-        st.sidebar.success("Login successful!")
+        st.sidebar.title("Experimental App")
+        if st.sidebar.button("Logout"):
+            with open("src/encryption/check_login", "wb") as f:
+                f.write(hashlib.md5("IS_LOGGED_OUT".encode()).digest())
+            st.experimental_rerun()
         menu = Menu()
         menu.display_option()
+    st.sidebar.write("Note: this is a collapsible sidebar.")
 
 
 if __name__ == "__main__":
