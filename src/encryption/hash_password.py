d92756094879dd62d1faef5081403cb2630a1d65
Doki064 <52833936+Doki064@users.noreply.github.com>
2020-12-09 17:26:40 +0700

    Rename

--- src/encryption/hash_password.py
+++ src/encryption/hash_password.py
@@ -15,7 +15,7 @@ def hash_password():
     hashed_pw = bcrypt.hashpw(
         base64.b64encode(hashlib.sha256(password).digest()),
         bcrypt.gensalt())
-    with open("encryption/hash_pw.txt", "wb+") as txt:
+    with open("encryption/hash_pw", "wb+") as txt:
         txt.write(hashed_pw)
         txt.close()
 
