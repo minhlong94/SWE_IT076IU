8f34b4153d38a30da03895f8231910273821040f
Doki064 <52833936+Doki064@users.noreply.github.com>
2020-12-13 00:26:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,10 +15,7 @@ def get_cached_id():
 
 
 def main():
-    session_state = SessionState.get(input_password='',
-                                     main_page=None,
-                                     menu=None
-                                     )
+    session_state = SessionState.get(welcome=False, input_password='', menu=None)
 
     cached_ids = get_cached_id()
     print(f"Session ID: {session_state.session_id}\t Cached ID: {cached_ids}\n")
@@ -28,16 +25,15 @@ def main():
 
     st.set_page_config(page_title="Wholesale Management System", layout="wide")
 
-    session_state.main_page = MainPage()
-    session_state.main_page.call()
+    MainPage().call()
 
     if session_state.session_id not in cached_ids.values():
-        st.sidebar.title("LOGIN SECTION")
-        st.sidebar.write(""" 
-                ## **WARNING: AUTHORIZED ACCESS ONLY**
-                Input your privileged password on the left sidebar, then click **Sign in** or press **Enter** to login.
-            """)
-        session_state.input_password = st.sidebar.text_input("Input administrator password: ", type="password",
+        st.sidebar.header("LOGIN SECTION")
+        st.sidebar.subheader("**WARNING: AUTHORIZED ACCESS ONLY**")
+        st.sidebar.write("""
+            Input your privileged password on the left sidebar, then click **Sign in** or press **Enter** to login.
+        """)
+        session_state.input_password = st.sidebar.text_input("Input privileged password: ", type="password",
                                                              value=session_state.input_password)
 
         if st.sidebar.button("Sign in") or session_state.input_password:
@@ -49,11 +45,20 @@ def main():
                 cached_ids[session_state.session_id] = session_state.session_id
                 st.experimental_rerun()
     else:
-        st.sidebar.title("Experimental App")
+        if not session_state.welcome:
+            st.balloons()
+            session_state.welcome = True
+
+        st.sidebar.header("APP OPTIONS")
+        st.sidebar.warning("This application is still in development state.")
+        st.sidebar.write(f"*Current session ID: {session_state.session_id}*")
+
         if st.sidebar.button("Sign out"):
             session_state.input_password = ''
             cached_ids.pop(session_state.session_id)
+            session_state.welcome = False
             st.experimental_rerun()
+
         session_state.menu = Menu()
         session_state.menu.display_option()
     st.sidebar.write("Note: this is a collapsible sidebar.")
