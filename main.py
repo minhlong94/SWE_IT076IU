696a1ab8f5f0508fc004b37e1b07484458c3cd62
Doki064 <52833936+Doki064@users.noreply.github.com>
2020-12-12 17:07:04 +0700

    Add get_session_id()
    Make get_session_id cache by streamlit.
    Max entries = 1. The cache will expire in 5 secs for demo and testing.

--- main.py
+++ main.py
@@ -1,21 +1,53 @@
-import atexit
 import base64
 import hashlib
 
 import bcrypt
 import streamlit as st
 from streamlit.report_thread import get_report_ctx
+from streamlit.server.server import Server
 
 from src.gui.main_page import MainPage
 from src.gui.menu import Menu
 
 
-def on_terminate():
-    with open("src/encryption/check_login", "wb") as f:
-        f.write(hashlib.md5("IS_LOGGED_OUT".encode()).digest())
+@st.cache(show_spinner=False, max_entries=1, ttl=5)
+def _get_session_id():
+    # Hack to get the session object from Streamlit.
+
+    ctx = get_report_ctx()
+
+    this_session = None
+
+    current_server = Server.get_current()
+    if hasattr(current_server, '_session_infos'):
+        # Streamlit < 0.56
+        session_infos = Server.get_current()._session_infos.values()
+    else:
+        session_infos = Server.get_current()._session_info_by_id.values()
+
+    for session_info in session_infos:
+        s = session_info.session
+        if (
+                # Streamlit < 0.54.0
+                (hasattr(s, '_main_dg') and s._main_dg == ctx.main_dg)
+                or
+                # Streamlit >= 0.54.0
+                (not hasattr(s, '_main_dg') and s.enqueue == ctx.enqueue)
+                or
+                # Streamlit >= 0.65.2
+                (not hasattr(s, '_main_dg') and s._uploaded_file_mgr == ctx.uploaded_file_mgr)
+        ):
+            this_session = s
+
+    if this_session is None:
+        raise RuntimeError(
+            "Oh noes. Couldn't get your Streamlit Session object. "
+            'Are you doing something fancy with threads?')
+
+    return str(id(this_session))
 
 
-def login_section():
+def _login_section():
     st.sidebar.title("LOGIN SECTION")
     st.sidebar.write(""" 
         ## **WARNING: AUTHORIZED ACCESS ONLY**
@@ -26,7 +58,7 @@ def login_section():
 
 
 def main():
-    ctx = get_report_ctx()
+    current_session_id = _get_session_id()
 
     with open("src/encryption/check_login", "rb") as f:
         check_login = f.readline()
@@ -38,27 +70,25 @@ def main():
     main_page = MainPage()
     main_page.call()
 
-    if check_login != hashlib.md5(ctx.session_id.encode()).digest():
-        input_password = login_section()
+    if check_login != hashlib.sha1(current_session_id.encode()).digest():
+        input_password = _login_section()
         if st.sidebar.button("Login") or input_password:
             if not bcrypt.checkpw(base64.b64encode(hashlib.sha256(input_password.encode()).digest()), hashed_password):
                 st.sidebar.warning("Wrong password!")
                 st.stop()
             else:
                 with open("src/encryption/check_login", "wb") as f:
-                    f.write(hashlib.md5(ctx.session_id.encode()).digest())
+                    f.write(hashlib.sha1(current_session_id.encode()).digest())
                 st.experimental_rerun()
     else:
         st.sidebar.title("Experimental App")
         if st.sidebar.button("Logout"):
             with open("src/encryption/check_login", "wb") as f:
-                f.write(hashlib.md5("IS_LOGGED_OUT".encode()).digest())
+                f.write(hashlib.sha1("IS_LOGGED_OUT".encode()).digest())
             st.experimental_rerun()
         menu = Menu()
         menu.display_option()
     st.sidebar.write("Note: this is a collapsible sidebar.")
-
-    atexit.register(on_terminate)
 
 
 if __name__ == "__main__":
