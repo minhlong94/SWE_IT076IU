764c65d511ff27d949573ac28dc42c0162abed15
Doki064 <52833936+Doki064@users.noreply.github.com>
2020-12-13 23:55:50 +0700

    Make pkg executable

--- run.py
+++
@@ -1,4 +0,0 @@
-import sys
-import subprocess
-
-subprocess.run([sys.executable, "-m", "pipenv", "run", "python", "main.py"])
--- run_demo.py
+++ run_demo.py
@@ -2,10 +2,9 @@ import sys
 
 from streamlit import cli
 
+from src.demo import app
 
 if __name__ == '__main__':
-    from src.demo import app
-
     filename = app.__file__
     sys.argv = ["streamlit", "run", filename]
     sys.exit(cli.main())
