2ff7fec915491026d0cda66222524e94ce6f4990
Doki064 <52833936+Doki064@users.noreply.github.com>
2020-12-16 07:55:39 +0700

    Update cli script

--- wms/cli.py
+++ wms/cli.py
@@ -1,9 +1,36 @@
 """This is a script which is run when the WMS package is executed."""
+import errno
+import os
 
 import click
 import streamlit.cli as stcli
+from wms import encryption
 
 from wms.hello import demo
+
+OUTPUT_DIR = os.path.expanduser(os.path.join("~", ".wms"))
+DATABASE_DIR = os.path.join(OUTPUT_DIR, "database")
+ENCRYPTION_KEY = os.path.join(OUTPUT_DIR, ".encryption")
+DATABASE_FILE = os.path.join(DATABASE_DIR, "WMS.db")
+
+
+def _create_output_dir():
+    check = False
+
+    if not os.path.exists(DATABASE_DIR):
+        try:
+            os.makedirs(DATABASE_DIR)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+        else:
+            check = True
+
+    if not os.path.exists(ENCRYPTION_KEY):
+        encryption.hash_password(encryption_file=ENCRYPTION_KEY)
+        check = True
+
+    return check
 
 
 @click.group()
@@ -16,7 +43,7 @@ def main(ctx, set_password):
     """
     from wms import encryption
 
-    _need_new_password = demo.create_output_dir()
+    _need_new_password = _create_output_dir()
     if _need_new_password and not set_password:
         if click.confirm("This is the first time you run this demo, do you want to set a new password?"):
             set_password = True
@@ -28,7 +55,7 @@ def main(ctx, set_password):
         if password == "python":
             if click.confirm("Continue with the default password?", default=True, abort=True):
                 pass
-        encryption.hash_password(password, encryption_file=demo.ENCRYPTION_KEY)
+        encryption.hash_password(password, encryption_file=ENCRYPTION_KEY)
 
 
 @main.command("hello")
--- wms/hello/demo.py
+++ wms/hello/demo.py
@@ -1,5 +1,4 @@
 import base64
-import errno
 import hashlib
 import os
 
@@ -8,33 +7,7 @@ import streamlit as st
 
 import wms
 from wms import SessionState
-from wms import encryption
-from wms.gui.main_page import MainPage
-from wms.gui.menu import Menu
-
-OUTPUT_DIR = os.path.expanduser(os.path.join("~", ".wms"))
-ENCRYPTION_KEY = os.path.join(OUTPUT_DIR, ".encryption")
-DATABASE_DIR = os.path.join(OUTPUT_DIR, "database")
-DATABASE_FILE = os.path.join(DATABASE_DIR, "WMS.db")
-
-
-def create_output_dir():
-    check = False
-
-    if not os.path.exists(DATABASE_DIR):
-        try:
-            os.makedirs(DATABASE_DIR)
-        except OSError as e:
-            if e.errno != errno.EEXIST:
-                raise
-        else:
-            check = True
-
-    if not os.path.exists(ENCRYPTION_KEY):
-        encryption.hash_password(encryption_file=ENCRYPTION_KEY)
-        check = True
-
-    return check
+from wms import gui
 
 
 @st.cache(allow_output_mutation=True, show_spinner=False, max_entries=1, ttl=300)
@@ -43,61 +16,49 @@ def _get_cached_id():
 
 
 def run(**kwargs):
-    session_state = SessionState.get(welcome=False, input_password='', menu=None)
-
-    cached_ids = _get_cached_id()
-    print(f"Session ID: {session_state.session_id}\t Cached ID: {cached_ids}\n")
+    session_state = SessionState.get(is_login=False, welcome=False)
 
     try:
         encryption_key = kwargs["encryption_file"]
         database_file = kwargs["database_file"]
     except KeyError:
-        encryption_key = ENCRYPTION_KEY
-        database_file = DATABASE_FILE
+        from wms import cli
+
+        encryption_key = cli.ENCRYPTION_KEY
+        database_file = cli.DATABASE_FILE
 
     with open(encryption_key, "rb") as f:
         hashed_password = f.read()
 
     st.set_page_config(page_title="Wholesale Management System", layout="wide")
 
-    MainPage().call()
+    main_page = gui.MainPage()
+    main_page.call()
 
-    if session_state.session_id not in cached_ids.values():
-        st.sidebar.header("LOGIN SECTION")
-        st.sidebar.subheader("**WARNING: AUTHORIZED ACCESS ONLY**")
-        st.sidebar.write("""
-            Input your privileged password on the left sidebar, then click **Sign in** or press **Enter** to login.
-        """)
-        session_state.input_password = st.sidebar.text_input("Input privileged password: ", type="password",
-                                                             value=session_state.input_password)
-
+    if not session_state.is_login:
+        gui.intro()
+        gui.login_section()
         if st.sidebar.button("Sign in") or session_state.input_password:
             if not bcrypt.checkpw(base64.b64encode(hashlib.sha512(session_state.input_password.encode()).digest()),
                                   hashed_password):
                 st.sidebar.warning("Wrong password!")
                 st.stop()
             else:
-                cached_ids[session_state.session_id] = session_state.session_id
+                session_state.is_login = True
                 st.experimental_rerun()
     else:
         if not session_state.welcome:
             st.balloons()
             session_state.welcome = True
 
-        st.sidebar.header("APP OPTIONS")
-        st.sidebar.warning("This application is still in development state.")
-        st.sidebar.write(f"*Current session ID: {session_state.session_id}*")
+        menu = gui.Menu(db_file=database_file,
+                        csv_zip=os.path.join(os.path.dirname(wms.__file__), "hello/dummy/dummy_data.zip"))
 
-        if st.sidebar.button("Sign out"):
-            session_state.input_password = ''
-            cached_ids.pop(session_state.session_id)
-            session_state.welcome = False
-            st.experimental_rerun()
+        menu.display_option()
+        gui.logout_section()
+        gui.info()
 
-        session_state.menu = Menu(db_file=database_file,
-                                  csv_zip=os.path.join(os.path.dirname(wms.__file__), "data/dummy/dummy_data.zip"))
-        session_state.menu.display_option()
-    st.sidebar.write("Note: this is a collapsible sidebar.")
+    session_state.sync()
 
 
 if __name__ == "__main__":
